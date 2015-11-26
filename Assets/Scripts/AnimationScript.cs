80ddb820a25c5c5ecf942ae5113fee6c626959b3
Thais Dias <dragonssaur@gmail.com>
2015-11-25 23:45:43 -0200

    Beta Version (With some bugs)

---
+++ Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationScript : MonoBehaviour {
+
+	public Vector3 vector;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.RotateAround (GameObject.Find ("Uranio235").transform.position, vector, 2);
+	}
+}
---
+++ Assets/Scripts/LevelManagment.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManagment : MonoBehaviour {
+
+	public void levelManager (string level) {
+		
+		Application.LoadLevel (level); 	
+	}
+}
